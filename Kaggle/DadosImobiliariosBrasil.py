93f0a588d5f1007769c5ab21c8c06e87b8e450a2
Carlos Eduardo <kadu.silva91@gmail.com>
2025-02-02 15:06:19 -0300

    Delete Kaggle directory

--- Kaggle/DadosImobiliariosBrasil.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib as mt
-import seaborn as sb
-
-#carregar o conjunto de dados
-df = pd.read_csv("C:\\Users\\Kadu\OneDrive\\Área de Trabalho\\Pasta.py\\Kaggle\\Dados\\ipeadata[13-06-2024-11-05].csv")
-
-#Exibir as primeiras linhas do DataFrame
-print(df.head())
